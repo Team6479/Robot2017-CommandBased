9c54d2cbe09a153a9b226cdf3dfc0afd1cd6b75d
ryraansh <ryraansh@gmail.com>
2017-10-13 13:36:11 -0700

    Commented out robotLogger

--- src/org/usfirst/frc/team6479/robot/Robot.java
+++ src/org/usfirst/frc/team6479/robot/Robot.java
@@ -9,7 +9,7 @@ import org.usfirst.frc.team6479.robot.subsystems.GearCollectorYControl;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import robotLogger.RobotLogger;
+// import robotLogger.RobotLogger;
 
 public class Robot extends IterativeRobot {
 
@@ -18,7 +18,7 @@ public class Robot extends IterativeRobot {
 	public static Climber climber;
 	public static GearCollectorWheels gwheels;
 	public static GearCollectorYControl gycont;
-	public RobotLogger logger;
+//	public RobotLogger logger;
 
 	@Override
 	public void robotInit() {
